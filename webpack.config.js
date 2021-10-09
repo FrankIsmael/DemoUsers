9ba8cdd2e8747195ce16533dc13a59a9326cebc5
Ismael <ismaelfcom93@gmail.com>
2021-10-09 00:11:21 -0500

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  devServer: {
+    port: 9000,
+    open: true,
+  },
+  entry: './src/index.tsx',
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript"
+            ],
+            plugins: [
+              [
+                '@babel/plugin-syntax-dynamic-import',
+                {
+                  "useESModules": true,
+                  "regenerator": false
+                }
+              ]
+            ]
+          }
+        },
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', 'ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    })
+  ]
+}
